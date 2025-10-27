ecaf7987b63504d4d464fb1d85f7c9a4eed10d56
Kyungmin Kim <sam2746@naver.com>
2025-10-27 18:04:37 +0900

    사용자에게 자동차 이름들과 시도 횟수를 입력받음, 콘솔 라이브러리 사용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       // 1. 자동차 이름 목록 입력
+        System.out.println("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분됩니다)");
+        String input = Console.readLine();
+
+        // 2. 시도 횟수 입력
+        System.out.println("1 이상의 시도할 횟수를 입력해주세요");
+        String attemptsInput = Console.readLine();
     }
 }
